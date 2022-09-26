ebb208c4d92ca64e9ce20fca20b3535794add64e
Ashley <IF 2203309@sit.singaporetech.edu.sg>
2022-09-26 11:08:59 +0800

    Added book lib

--- firstcode.py
+++ firstcode.py
@@ -1,5 +1,13 @@
 
 import sys
 import time
+import lib_book as lb203309/test$ git push https://$(cat ~/personalToken.txt)@github.com/ashleylinghq/test.git
+Enumerating objects: 4, done.
+Counting objects: 100% (4/4), done.
+203309/test$ git push https://$(cat ~/personalToken.txt)@github.com/ashleylinghq/test.git
+Enumerating objects: 4, done.
+Counting objects: 100% (4/4), done.
+
 
 print("Good day & Hello Ashley")
+lb.types("Electronics")
